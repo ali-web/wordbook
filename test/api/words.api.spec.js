8b3c80a60c185a47befab625fdbdea70c47e0b79
Ali Dehghan <aliweb.ir@gmail.com>
2018-09-30 19:45:57 -0400

    issue-7: removing unused code

--- test/api/words.api.spec.js
+++ test/api/words.api.spec.js
@@ -15,25 +15,5 @@ describe('/words Api tests', () => {
             expect(res.body).to.be.an('array');
             console.log(res.body);
         });
-    });
-
-    // it('PUT /user should return a string', () => {
-    //     return supertest(app)
-    //     .put('/user')
-    //     .expect(200)
-    //     .then(res => {
-    //         expect(res.body).to.be.an('string');
-    //         console.log(res.body);
-    //     });
-    // });    
-
-    // it('GET / should return a json object', () => {
-    //     return supertest(app)
-    //     .get('/')
-    //     .expect(200)
-    //     .then(res => {
-    //         expect(res.body).to.be.an('object');
-    //         console.log(res.body);
-    //     });
-    // });    
+    });   
 });
